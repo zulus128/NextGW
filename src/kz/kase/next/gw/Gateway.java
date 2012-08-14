42074b7624827c6611ebc833a4c94c5ec6ac157e
Vadim <vgzulus@gmail.com>
2012-08-14 12:16:03 +0600

    y123

---
+++ src/kz/kase/next/gw/Gateway.java
@@ -0,0 +1,11 @@
+package kz.kase.next.gw;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vadim
+ * Date: 8/14/12
+ * Time: 12:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Gateway {
+}
